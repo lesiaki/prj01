7e14bcdd814ee5a482efd0c38173d98a2ba6db80
Francis Sowani <frathoso@gmail.com>
2014-03-18 19:58:44 +0400

    Add database create & upgrade functions

--- src/com/example/vehicle/DatabaseHelper.java
+++ src/com/example/vehicle/DatabaseHelper.java
@@ -1,5 +1,43 @@
 package com.example.vehicle;
 
-public class DatabaseHelper{
-	
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+	// Constants for the database details
+	private static final int 	DB_VERSION = 1;
+	private static final String DB_NAME = "app.db";
+	private static final String TABLE_USERS = "users";
+
+	// Queries for creating tables
+	// USERS Table
+	private static final String SQL_USERS_CREATE = "CREATE TABLE "
+			+ TABLE_USERS 
+			+ "(" 
+			+ "user_id INTEGER PRIMARY KEY AUTOINCREMENT ,"
+			+ "username VARCHAR(45) NOT NULL ," 
+			+ "password VARCHAR(45) NOT NULL ,"
+			+ ")";
+
+	// A general SQL command for deleting a table
+	private static final String SQL_DROP = "DROP TABLE IF EXISTS ";
+
+	// Constructor
+	public DatabaseHelper(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// Create the tables if they do not exist
+		db.execSQL(SQL_USERS_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Simply replaces the old version of the database tables with the new one
+		db.execSQL(SQL_DROP + TABLE_USERS);
+		onCreate(db);
+	}
 }
